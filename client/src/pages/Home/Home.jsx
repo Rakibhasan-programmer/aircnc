9d2f862f78735cdac8f5db972a7f1187721d8756
unknown <rakibulhasan.seu.cse@gmail.com>
2023-10-29 05:16:54 +0600

    dynamic category wise data load done

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import Categories from '../../Components/Categories/Categories';
+import React from "react";
+import Categories from "../../Components/Categories/Categories";
+import Rooms from "../../Components/Rooms/Rooms";
 
 const Home = () => {
-    return (
-        <Categories />
-    );
+  return (
+    <>
+      <Categories />
+      <Rooms />
+    </>
+  );
 };
 
 export default Home;
