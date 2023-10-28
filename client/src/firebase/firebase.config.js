19b5d25c93a1935202e955d4a428a31478a5a07f
unknown <rakibulhasan.seu.cse@gmail.com>
2023-10-28 09:32:15 +0600

    basic setup with authentication

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCR3TVlWC_8MfRfZa5OTmkXxeKq3gpFnh4",
+  authDomain: "aircnc-cede8.firebaseapp.com",
+  projectId: "aircnc-cede8",
+  storageBucket: "aircnc-cede8.appspot.com",
+  messagingSenderId: "3297714534",
+  appId: "1:3297714534:web:1b418295afe74a73ad68a7",
+};
+
+export const app = initializeApp(firebaseConfig);
