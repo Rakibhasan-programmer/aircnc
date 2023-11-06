f355917e9ddee9bdc83be96500633660c49f151e
unknown <rakibulhasan.seu.cse@gmail.com>
2023-11-06 23:43:02 +0600

    some issues fixed

--- client/src/pages/RoomDetails/RoomDetails.jsx
+++ client/src/pages/RoomDetails/RoomDetails.jsx
@@ -8,7 +8,7 @@ const RoomDetails = () => {
   return (
     <Container>
       <div className="max-w-screen-lg mx-auto">
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-5">
           <div>
             <Header />
           </div>
